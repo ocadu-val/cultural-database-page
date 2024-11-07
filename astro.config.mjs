350ae03f6e5d611c7dc9d1e478fccac6b4b8ee0c
Juan Sulca <juanm.sulca@gmail.com>
2024-11-07 10:55:18 -0500

    Added github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://ocadu-val.github.io',
+  base: 'cultural-database-page',
 });
